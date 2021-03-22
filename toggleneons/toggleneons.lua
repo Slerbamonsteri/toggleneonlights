eac2d0ce34d90a3f1a6cdc61ad3c477c22a04fef
Slerbamonsteri <75551384+Slerbamonsteri@users.noreply.github.com>
2021-03-22 04:09:32 +0200

    Update toggleneons.lua
    Added check so only driver can toggle neons

--- toggleneons/toggleneons.lua
+++ toggleneons/toggleneons.lua
@@ -13,27 +13,29 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(10)
         local ped = PlayerPedId()
-        if IsPedSittingInAnyVehicle(ped) then
+        local vehicle = GetVehiclePedIsIn(ped, false)
+		local driver  = GetPedInVehicleSeat(vehicle, -1)
+        local alavalotkiinni = IsVehicleNeonLightEnabled(vehicle, 1)
+		
+        if IsPedInVehicle(ped,vehicle, true) and driver == ped then
             if IsControlPressed(1, 132) and IsControlJustPressed(1, 47) then
-                local vehicle = GetVehiclePedIsIn(ped)
-                local alavalotkiinni = IsVehicleNeonLightEnabled(vehicle, 1)
 				if alavalotkiinni then
 					if alavalot == false then
 						alavalot = true
 						DisableVehicleNeonLights(vehicle, true)
-						TriggerEvent('mythic_notify:client:SendAlert', { type = 'error', length = 3000, text = 'Neonlights : OFF' })
-						--ESX.ShowNotification('Neonlights: ~r~OFF')
+						--TriggerEvent('mythic_notify:client:SendAlert', { type = 'error', length = 3000, text = 'Neonlights : OFF' })
+						ESX.ShowNotification('Neonlights: ~r~OFF')
 						Wait(2000)
 					elseif alavalot == true then
 						alavalot = false
 						DisableVehicleNeonLights(vehicle, false)
-						TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 3000, text = 'Neonlights : ON' })
-						--ESX.ShowNotification('Neonlights: ~g~ON')
+						--TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 3000, text = 'Neonlights : ON' })
+						ESX.ShowNotification('Neonlights: ~g~ON')
 						Wait(2000)
 					end
 				else
-					TriggerEvent('mythic_notify:client:SendAlert', { type = 'error', length = 5000, text = 'You need to have installed Neonlights?' })
-					--ESX.ShowNotification('You need to have installed Neonlights')
+					--TriggerEvent('mythic_notify:client:SendAlert', { type = 'error', length = 5000, text = 'You need to have installed Neonlights?' })
+					ESX.ShowNotification('You need to have installed Neonlights')
 					Wait(2000)
 				end
             end
